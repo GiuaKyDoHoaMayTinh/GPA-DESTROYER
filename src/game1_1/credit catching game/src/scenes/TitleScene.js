d7c46bc22d7a3abeefd8d5797eed975939e060f0
24023082-cell <24023082@vnu.edu.vn>
2026-05-03 12:26:59 +0700

    Revert "Update TitleScene.js"
    This reverts commit 93f8a43e714bf27c0d8529c7ba1d96be5934fe64.

--- src/game1_1/credit catching game/src/scenes/TitleScene.js
+++ src/game1_1/credit catching game/src/scenes/TitleScene.js
@@ -16,8 +16,6 @@ export class TitleScene extends Phaser.Scene {
         this.load.atlas('Subjects','assets/spritesheets/Subjects.png','assets/spritesheets/subject_spritesheet.json');
         this.load.audio('pop', 'assets/audio/pop.mp3');
         this.load.audio('xtremefreddy-game-music-loop-5-144569', 'assets/audio/xtremefreddy-game-music-loop-5-144569.mp3');
-        this.load.audio('lose', 'assets/audio/lose.mp3');
-        this.load.audio('win', 'assets/audio/win.mp3');
     }
 
 
@@ -43,7 +41,7 @@ export class TitleScene extends Phaser.Scene {
 
         this.cameras.main.fadeIn(500);
         this.sound.play('xtremefreddy-game-music-loop-5-144569', {
-            volume: 0.3,
+            volume: 0.5,
             loop: true
         });
     }
